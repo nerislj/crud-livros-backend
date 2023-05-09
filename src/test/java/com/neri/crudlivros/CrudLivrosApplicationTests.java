d61c5f8687a7fe8a57db49a88bef51abb4a570cc
nerijunior <nerijunior@DETRANSC-005943.mshome.net>
2023-05-09 10:56:10 -0300

    commit initial

---
+++ src/test/java/com/neri/crudlivros/CrudLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neri.crudlivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
