c9ceaa2a510dd16c13351416740192799df7507c
nerijunior <nerijunior@host.docker.internal>
2023-05-10 10:23:24 -0300

    findAll categoria padrao DTO

--- src/main/java/com/neri/crudlivros/resources/CategoriaResource.java
+++ src/main/java/com/neri/crudlivros/resources/CategoriaResource.java
@@ -1,5 +1,8 @@
 package com.neri.crudlivros.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.neri.crudlivros.domain.Categoria;
+import com.neri.crudlivros.dtos.CategoriaDTO;
 import com.neri.crudlivros.service.CategoriaService;
 
 @RestController
@@ -23,6 +27,13 @@ public class CategoriaResource {
 		Categoria obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 
+	}
+
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 
 }
--- src/main/java/com/neri/crudlivros/service/CategoriaService.java
+++ src/main/java/com/neri/crudlivros/service/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.neri.crudlivros.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,12 @@ public class CategoriaService {
 		Optional<Categoria> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado Id: " + id + ", Tipo: " + Categoria.class.getName()));
+
+	}
+
+	public List<Categoria> findAll() {
+
+		return repository.findAll();
 
 	}
 
