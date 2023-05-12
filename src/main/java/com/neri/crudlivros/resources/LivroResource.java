596bff01992324f0e78d15fa002939b0abf1d10f
nerijunior <nerijunior@host.docker.internal>
2023-05-12 10:46:50 -0300

    listando todos livros por categoria

--- src/main/java/com/neri/crudlivros/resources/LivroResource.java
+++ src/main/java/com/neri/crudlivros/resources/LivroResource.java
@@ -1,13 +1,18 @@
 package com.neri.crudlivros.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.neri.crudlivros.domain.Livro;
+import com.neri.crudlivros.dtos.LivroDTO;
 import com.neri.crudlivros.service.LivroService;
 
 @RestController
@@ -21,6 +26,14 @@ public class LivroResource {
 	public ResponseEntity<Livro> findById(@PathVariable Integer id){
 		Livro obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<LivroDTO>> findAll(@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat){
+		List<Livro> list = service.findAll(id_cat);
+		List<LivroDTO> listDTO = list.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+		
 	}
 	
 }
